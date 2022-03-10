6a78b2390e27f7894f7b065ac9cea4237d48ecc2
Juan Pedraza <juan_pedraza@outlook.com>
2022-03-10 14:08:42 -0500

    Día 7

--- Día7/atributos.py
+++ Día7/atributos.py
@@ -1,10 +1,31 @@
 class Bird:
 
+    #Atributos de clase
+    alas = True
+
+    #Atributos de instancia
     def __init__(self,color, especie):
         self.color = color
         self.especie = especie
 
+    #Métodos de instancia
+    def sound(self):
+        print(f'El pájaro hace algún sonido y es de color {self.color}')
+
+    def volar(self,metros):
+        print(f'El ave voló {metros} metros')
+        #Método de instancia
+        self.sound()
+    @classmethod
+
+    def poner_huevos(cls,cantidad):
+        print(f'El ave puso {cantidad} huevos')
+
+
+
+
 
 my_bird = Bird('Verde','Águila')
 
-print(f'Está ave es un {my_bird.especie} y es de color {my_bird.color}')
+
+my_bird.volar(90)
