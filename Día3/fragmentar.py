de31b38df339f4bc398c5a95dea5a35ffc6ba357
Juan Pedraza <juan_pedraza@outlook.com>
2022-06-02 15:31:59 -0500

    Día 3

--- Día3/fragmentar.py
+++ Día3/fragmentar.py
@@ -1,7 +1,7 @@
 texto = "Este texto es un poco largo para la prueba"
 # Los corchetes indican inicio y fin de la cadena de texto
-fragmento = texto[2:10]
-print(fragmento)
+frag = texto[2:10]
+print(frag)
 
 #El tercer argumento en los corchetes cuadrados indica el salto de cada caracter
 fragmento = texto[2:20:2]
