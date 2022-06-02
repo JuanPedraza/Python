92e92c98afadebe8ea0ed9bdec30a6de2ba1d389
Juan Pedraza <juan_pedraza@outlook.com>
2022-06-02 15:30:23 -0500

    Día 2

--- Día2/ejercicio.py
+++ Día2/ejercicio.py
@@ -1,8 +1,8 @@
-"""Pedir nombre, pedir ingreso en ventas y calcylar las ganancias"""
+"""Pedir nombre, pedir ingreso en ventas y calcular las ganancias"""
 
 name = input("¿Cuál es tu nombre?: ")
 sales = float(input("Escribe el valor de tus ventas: "))
 porcentaje = 13
-total_ventas = round(sales*porcentaje/100,2)
+total_ganancias = round(sales*porcentaje/100,2)
 
-print(f"Hola {name} el total de tus ganancias son de {total_ventas}")
+print(f"Hola {name} el total de tus ganancias son de {total_ganancias}")
