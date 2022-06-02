67cb7d72cea8e0f6cc4c80d893862c4ba8d533a4
Juan Pedraza <juan_pedraza@outlook.com>
2022-06-02 15:23:26 -0500

    Día 4

---
+++ Día4/test.py
@@ -0,0 +1,11 @@
+mascotas = ['Perro', 'Gato','Conejo','pez']
+
+for mascota in mascotas:
+    if mascota == 'Perro':
+        print('Tienes un perro')
+    elif mascota == "Gato":
+        print('tienes un gato')
+    elif mascota == 'Conejo':
+        print('tienes un conejo')
+    else:
+        print('Tienes un pez')
