4a4f86bd1d98915be46abfac0dc7ecc7714bd745
Juan Pedraza <juan_pedraza@outlook.com>
2022-06-02 15:33:36 -0500

    Día 6

--- Día6/escribir_archivos.py
+++ Día6/escribir_archivos.py
@@ -1,8 +1,8 @@
-lista = ['Hola', 'mundo', 'haciendo', 'pruebas']
+mi_lista = ['Hola', 'mundo', 'haciendo', 'pruebas']
 
 archivo = open('texto1.txt','w')
 
-for p in lista:
+for p in mi_lista:
     archivo.writelines(p + '\n')
 
 
