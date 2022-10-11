a1755347525f68325ce8c1807a2687e496d18fcf
Juan Pedraza <juan_pedraza@outlook.com>
2022-10-11 16:56:29 -0500

    Día 6

--- Día6/pathlib.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-carpeta = Path('/home/juanpedraza/Documentos/Python/Día6/Directorios/texto.txt')
-
-print(carpeta.suffix)
---
+++ Día6/pathlib_lib.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+
+carpeta = Path('home/juanpedraza/Documentos/Python/venv/bin/python /home/juanpedraza/Documentos/Python/Día6/Directorios/texto.txt')
+
+print(carpeta.suffix)
+
